ca8ad2c76e18d1cf63181da0c9516e914787e1ed
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-05-25 15:27:21 -0500

    Refactor as functional component with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,102 +4,56 @@ import Button from './Button';
 import Result from './Result';
 import calculate from '../logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    const { previous, operand, current } = props;
-    this.state = {
-      total: previous,
-      operation: operand,
-      next: current,
-    };
-    this.update = this.update.bind(this);
-  }
+const Calculator = (props) => {
+  const { previous, operand, current } = props;
+  const [total, setTotal] = React.useState(previous);
+  const [operation, setOperation] = React.useState(operand);
+  const [next, setNext] = React.useState(current);
 
-  update(buttonPress) {
-    const calculation = calculate(this.state, buttonPress);
-    this.setState(() => ({
-      total: calculation.total,
-      operation: calculation.operation,
-      next: calculation.next,
-    }));
-  }
+  const update = (buttonPress) => {
+    const calculation = calculate({ total, operation, next }, buttonPress);
+    setTotal(calculation.total);
+    setOperation(calculation.operation);
+    setNext(calculation.next);
+  };
 
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div className="grid-container">
-        <Result id="result" text="0" previous={total} operand={operation} current={next} />
+  const buttons = [
+    { id: 'all-clear', className: 'digit', text: 'AC' },
+    { id: 'plus-minus', className: 'digit', text: '±' },
+    { id: 'mod', className: 'digit', text: '%' },
+    { id: 'division', className: 'operation', text: '÷' },
+    { id: 'n7', className: 'digit', text: '7' },
+    { id: 'n8', className: 'digit', text: '8' },
+    { id: 'n9', className: 'digit', text: '9' },
+    { id: 'multiplication', className: 'operation', text: '×' },
+    { id: 'n4', className: 'digit', text: '4' },
+    { id: 'n5', className: 'digit', text: '5' },
+    { id: 'n6', className: 'digit', text: '6' },
+    { id: 'subtraction', className: 'operation', text: '-' },
+    { id: 'n1', className: 'digit', text: '1' },
+    { id: 'n2', className: 'digit', text: '2' },
+    { id: 'n3', className: 'digit', text: '3' },
+    { id: 'addition', className: 'operation', text: '+' },
+    { id: 'n0', className: 'digit', text: '0' },
+    { id: 'point', className: 'digit', text: '.' },
+    { id: 'equal', className: 'operation', text: '=' },
+  ];
 
+  return (
+    <div className="grid-container">
+      <Result id="result" text="0" previous={total} operand={operation} current={next} />
+      {buttons.map((btn) => (
         <Button
-          id="all-clear"
-          className="digit"
-          text="AC"
-          buttonClick={(string) => this.update(string)}
+          key={btn.id}
+          id={btn.id}
+          className={btn.className}
+          text={btn.text}
+          buttonClick={(string) => update(string)}
         />
-        <Button
-          id="plus-minus"
-          className="digit"
-          text="±"
-          buttonClick={(string) => this.update(string)}
-        />
-        <Button
-          id="percent"
-          className="digit"
-          text="%"
-          buttonClick={(string) => this.update(string)}
-        />
-        <Button
-          id="division"
-          className="operation"
-          text="÷"
-          buttonClick={(string) => this.update(string)}
-        />
-        <Button id="n7" className="digit" text="7" buttonClick={(string) => this.update(string)} />
-        <Button id="n8" className="digit" text="8" buttonClick={(string) => this.update(string)} />
-        <Button id="n9" className="digit" text="9" buttonClick={(string) => this.update(string)} />
-        <Button
-          id="multiplication"
-          className="operation"
-          text="×"
-          buttonClick={(string) => this.update(string)}
-        />
-        <Button id="n4" className="digit" text="4" buttonClick={(string) => this.update(string)} />
-        <Button id="n5" className="digit" text="5" buttonClick={(string) => this.update(string)} />
-        <Button id="n6" className="digit" text="6" buttonClick={(string) => this.update(string)} />
-        <Button
-          id="subtraction"
-          className="operation"
-          text="-"
-          buttonClick={(string) => this.update(string)}
-        />
-        <Button id="n1" className="digit" text="1" buttonClick={(string) => this.update(string)} />
-        <Button id="n2" className="digit" text="2" buttonClick={(string) => this.update(string)} />
-        <Button id="n3" className="digit" text="3" buttonClick={(string) => this.update(string)} />
-        <Button
-          id="addition"
-          className="operation"
-          text="+"
-          buttonClick={(string) => this.update(string)}
-        />
-        <Button id="n0" className="digit" text="0" buttonClick={(string) => this.update(string)} />
-        <Button
-          id="point"
-          className="digit"
-          text="."
-          buttonClick={(string) => this.update(string)}
-        />
-        <Button
-          id="equal"
-          className="operation"
-          text="="
-          buttonClick={(string) => this.update(string)}
-        />
-      </div>
-    );
-  }
-}
+      ))}
+    </div>
+  );
+};
 
 Calculator.propTypes = {
   previous: PropTypes.string.isRequired,
