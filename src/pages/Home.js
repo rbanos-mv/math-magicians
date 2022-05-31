ff4ba79862f2b4a4500a206fa14f9a37920c7876
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-05-31 14:09:46 -0500

    Create Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="home">
+    <h2>Welcome to our page!</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ultricies mi quis hendrerit dolor magna eget. Adipiscing elit
+      pellentesque habitant morbi tristique senectus. Velit euismod in pellentesque massa placerat
+      duis ultricies lacus. Amet facilisis magna etiam tempor orci eu lobortis elementum nibh.
+      Tellus elementum sagittis vitae et leo duis ut diam quam. Porttitor massa id neque aliquam
+      vestibulum. Enim diam vulputate ut pharetra sit. Enim eu turpis egestas pretium aenean
+      pharetra. Cras tincidunt lobortis feugiat vivamus at augue eget. Duis convallis convallis
+      tellus id interdum velit laoreet id donec.
+    </p>
+    <p>
+      In hac habitasse platea dictumst vestibulum. Aliquet porttitor lacus luctus accumsan.
+      Scelerisque purus semper eget duis at tellus at. In fermentum et sollicitudin ac orci
+      phasellus egestas tellus. Cursus turpis massa tincidunt dui ut ornare. Id leo in vitae turpis
+      massa sed elementum tempus egestas. Vestibulum mattis ullamcorper velit sed ullamcorper morbi
+      tincidunt ornare massa. Ultrices eros in cursus turpis massa tincidunt dui. Iaculis at erat
+      pellentesque adipiscing commodo elit. Vitae ultricies leo integer malesuada nunc vel. Massa
+      massa ultricies mi quis hendrerit dolor magna. Nibh tortor id aliquet lectus proin nibh nisl.
+      Est sit amet facilisis magna etiam tempor orci eu. Ut sem viverra aliquet eget sit amet
+      tellus. Pharetra massa massa ultricies mi quis hendrerit dolor magna.
+    </p>
+  </div>
+);
+
+export default Home;
