83ca51e0f60d6c44961f57480360899e2bb15ace
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-05-31 14:10:28 -0500

    Add pages and router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
 import './App.css';
-import Calculator from './components/Calculator';
+import Home from './pages/Home';
+import CalcPage from './pages/CalcPage';
+import Quote from './pages/Quote';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,7 +14,32 @@ class App extends React.Component {
   }
 
   render() {
-    return <Calculator previous="" operand="" current="0" solved={false} />;
+    return (
+      <Router>
+        <header>
+          <h1>Math Magicians</h1>
+          <div className="nav">
+            <nav>
+              <Link className="link" to="/">
+                Home
+              </Link>
+              <Link className="link" to="/calculator">
+                Calculator
+              </Link>
+              <Link className="link" to="/quote">
+                Quote
+              </Link>
+            </nav>
+          </div>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<CalcPage />} />
+          <Route path="/quote" element={<Quote />} />
+          <Route path="*" element={<div>Page not found</div>} />
+        </Routes>
+      </Router>
+    );
   }
 }
 
